70d13bc1dab1712d62c4420d36924e8fd4c58b08
Md-MahaburRahmanNadim <mdnadimhaaan120@gmail.com>
2026-07-05 15:52:27 +0600

    learnign about function and how to use them

--- demo/index.php
+++ demo/index.php
@@ -7,24 +7,78 @@
 </head>
 <body>
     <?php 
-    $books = [
-        'Al Quran',
-        "Hadis",
-        'Din',
-        "Sunnah"
-    ]
-   
+
+    /**
+     * we will start with next day at 1hr 5 mint inshallah to 2 hr 5 mint
+     */
     
+    $books = [
+     [
+        'author'=> 'allah',
+        'name'=>'al quran',
+        'publishYear' => 1450,
+        'url'=> 'Hazrat Mohamad (S. A.)'
+     ]
+     , [
+        'author'=> 'Nobi Mohamad (S.A.)',
+        'name'=>'Sunnah',
+        'publishYear' => 1450,
+        'url'=> 'Hazrat Mohamad (S. A.)'
+     ], [
+        'author'=> 'allah',
+        'name'=>'al quran',
+        'publishYear' => 1450,
+        'url'=> 'Hazrat Mohamad (S. A.)'
+     ], [
+        'author'=> 'allah',
+        'name'=>'al quran',
+        'publishYear' => 1450,
+        'url'=> 'Hazrat Mohamad (S. A.)'
+     ]
+    ];
+
+    function filterByName($books,$author):Array{
+        $filterBooks =[];
+        foreach($books as $book){
+            if($book['author'] ===$author){
+                $filterBooks[] = $book;
+                /* need to learn 
+
+                // different between 
+                // $filterBooks = $book;
+                *$filterBooks[] = $book;
+                */
+            }
+            
+        }
+        return $filterBooks;
+    }
+    $author = 'allah';
     ?>
       <ul>
     
         <?php foreach($books as $book):?>
            
-                <li><?= $book?></li>
+                <li>
+                    <?= $book['name']?>
+            
+            </li>
+
             
     
         <?php endforeach ?>
+
        </ul>
+       <h3>only book for <?= $author?></h3>
+       <ul>
+        <?php
+            foreach(filterByName($books,$author) as $book):?>
+            <li> <?=$book['url'] ?></li>
+
+            <?php endforeach ?>
+        
+       </ul>
+    
     
 </body>
 </html>
