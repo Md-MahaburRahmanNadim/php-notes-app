404d3b341bb4c493e1c7585c1ce11ca157672647
Md-MahaburRahmanNadim <mdnadimhaaan120@gmail.com>
2026-07-12 21:34:41 +0600

    seperating appliciaon an view layer

--- demo/index.php
+++ demo/index.php
@@ -1,12 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <?php 
+ <?php 
 
     /**
      * we will start with next day at 1hr 5 mint inshallah to 2 hr 5 mint
@@ -36,57 +28,10 @@
         'url'=> 'Hazrat Mohamad (S. A.)'
      ]
     ];
-// anonumas or lambda fn
-//    $filterByAuthor = function ($books,$author):Array{
-// more generic filter fn
-// $filter = function ($items,$fn){
-        // $filterItems =[];
-        // foreach($items as $item){
-            // if($fn($item)){
-                // $filterItems[] = $item;
-                // /* need to learn 
-// 
-                // different between 
-                // $filterBooks = $book;
-                // *$filterBooks[] = $book;
-                // */
-            // }
-            // 
-        // }
-        // return $filterItems;
-    // };
 
     $filteredBooks = array_filter( $books,function($book){
         return $book['author'] === 'allah';
 
     });
+    include_once './index.view.php'
     ?>
-      <ul>
-    
-        <?php foreach($books as $book):?>
-           
-                <li>
-                    <?= $book['name']?>
-            
-            </li>
-
-            
-    
-        <?php endforeach ?>
-
-       </ul>
-       <h3>only book for <?= $author?></h3>
-       <ul>
-        <?php
-            // foreach(filterByAuthor($books,$author) as $book):
-            foreach($filteredBooks as $book):?>
-            <!-- extracted variable -->
-            <li> <?=$book['url'] ?></li>
-
-            <?php endforeach ?>
-        
-       </ul>
-    
-    
-</body>
-</html>
