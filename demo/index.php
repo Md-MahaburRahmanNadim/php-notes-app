8422bb0ce037b7b7b1f515de311d3f5093b71bed
Md-MahaburRahmanNadim <mdnadimhaaan120@gmail.com>
2026-07-13 16:29:26 +0600

    route is done

--- demo/index.php
+++ demo/index.php
@@ -1,34 +1,6 @@
 <?php
 include_once 'functions.php';
-// dd($_SERVER['REQUEST_URI']);
-$fullUri = $_SERVER['REQUEST_URI'];
-// to get only url (path) not the query parms
-$uri = parse_url($fullUri)['path'];
-
-$routes =[
-    '/'=>'controllers/index.php',   
-    '/about'=>'controllers/about.php',
-    '/contract'=>'controllers/contract.php'
-    ];
-
-function abort($code){
-    http_response_code($code=404);
-    require_once "views/{$code}.php";
-    die();
-}
-function routeToController($uri,$routes){
-    if(array_key_exists($uri,$routes)){
-
-        require_once $routes[$uri];
-
-    }else{
-
-        abort();
-
-    }
-
-}
-routeToController($uri,$routes);
+include_once 'router.php';
 
 
 
--- demo/router.php
+++ demo/router.php
@@ -1,1 +1,30 @@
 <?php
+// dd($_SERVER['REQUEST_URI']);
+$fullUri = $_SERVER['REQUEST_URI'];
+// to get only url (path) not the query parms
+$uri = parse_url($fullUri)['path'];
+
+$routes =[
+    '/'=>'controllers/index.php',   
+    '/about'=>'controllers/about.php',
+    '/contract'=>'controllers/contract.php'
+    ];
+
+function abort($code=404 ){
+    http_response_code($code);
+    require_once "views/{$code}.php";
+    die();
+}
+function routeToController($uri,$routes){
+    if(array_key_exists($uri,$routes)){
+
+        require_once $routes[$uri];
+
+    }else{
+
+        abort();
+
+    }
+
+}
+routeToController($uri,$routes);
