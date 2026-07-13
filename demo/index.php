c00736558bfacbf4c4004709a347df23b7444405
Md-MahaburRahmanNadim <mdnadimhaaan120@gmail.com>
2026-07-13 22:35:38 +0600

    basic refectring of db connectiON is done with class

--- demo/index.php
+++ demo/index.php
@@ -5,22 +5,29 @@ include_once 'functions.php';
 // code refactoring
 // connect to a db and execute a query
 class Database{
-    public function __constract(){
+    // what if we can create pdo obj once and user rest of the instance creation
+    public $connection;
+    public function __construct(){
+        $dns = 'mysql:host=localhost;port=3306;user=laravel_user;dbname=laravel_db;charset=utf8mb4;password=Secret123!;';
+// $pdo = new PDO($dns);
+$this->$connection = new PDO($dns);
 
     }
-    public function query(){
-$dns = 'mysql:host=localhost;port=3306;user=laravel_user;dbname=laravel_db;charset=utf8mb4;password=Secret123!;';
-$pdo = new PDO($dns);
-$statement = $pdo->prepare('select * from posts');
+    public function query($query){
+
+$statement = $this->$connection->prepare($query);
 $statement->execute();
 // $posts = $statement->fetchAll();
-return $statement->fetchAll(PDO::FETCH_ASSOC);
+return $statement;
     }
     
 
 }
 $db= new Database();
-$posts = $db->query();
+
+// $posts = $db->query('select * from posts')->fetchAll(PDO::FETCH_ASSOC);
+$post = $db->query('select * from posts')->fetch(PDO::FETCH_ASSOC);
+// dd($posts);
 
 
 
