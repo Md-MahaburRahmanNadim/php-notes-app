932d2190eec7e0d5de4fc4a1bbb04fec4263c6d2
Md-MahaburRahmanNadim <mdnadimhaaan120@gmail.com>
2026-07-13 15:20:56 +0600

    route basic refactoring

--- demo/index.php
+++ demo/index.php
@@ -1,11 +1,25 @@
 <?php
 include_once 'functions.php';
 // dd($_SERVER['REQUEST_URI']);
-$uri = $_SERVER['REQUEST_URI'];
-if($uri === '/'){
-    require_once './controllers/index.php';
-}elseif($uri === '/about'){
-    require_once './controllers/about.php';
-}elseif($uri === '/contract'){
-    require_once './controllers/contract.php';
-}
+$fullUri = $_SERVER['REQUEST_URI'];
+// to get only url (path) not the query parms
+$uri = parse_url($fullUri)['path'];
+// dd(parse_url($uri)['path']);
+
+// if($uri === '/'){
+//     require_once './controllers/index.php';
+// }elseif($uri === '/about'){
+//     require_once './controllers/about.php';
+// }elseif($uri === '/contract'){
+//     require_once './controllers/contract.php';
+// }
+$routes =[
+    '/'=>'controllers/index.php',   
+    '/about'=>'controllers/about.php',
+    '/contract'=>'controllers/contract.php'
+    ];
+
+    // if(array_key_exists($key,$routes))
+    if(array_key_exists($uri,$routes)){
+        require_once $routes[$uri];
+    }
