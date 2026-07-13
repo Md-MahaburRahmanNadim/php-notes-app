f733fa8476372f9056ae80ede061cbf56bd318c7
Md-MahaburRahmanNadim <mdnadimhaaan120@gmail.com>
2026-07-13 22:15:58 +0600

    basic refectring of db connection

--- demo/index.php
+++ demo/index.php
@@ -2,12 +2,27 @@
 include_once 'functions.php';
 // include_once 'router.php';
 
+// code refactoring
+// connect to a db and execute a query
+class Database{
+    public function __constract(){
+
+    }
+    public function query(){
 $dns = 'mysql:host=localhost;port=3306;user=laravel_user;dbname=laravel_db;charset=utf8mb4;password=Secret123!;';
 $pdo = new PDO($dns);
 $statement = $pdo->prepare('select * from posts');
 $statement->execute();
 // $posts = $statement->fetchAll();
-$posts = $statement->fetchAll(PDO::FETCH_ASSOC);
+return $statement->fetchAll(PDO::FETCH_ASSOC);
+    }
+    
+
+}
+$db= new Database();
+$posts = $db->query();
+
+
 
 // dd($posts);
 foreach ($posts as $post) :?>
