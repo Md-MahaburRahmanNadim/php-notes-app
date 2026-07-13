be0153b49c8c7faf1817ed54633625935054a116
Md-MahaburRahmanNadim <mdnadimhaaan120@gmail.com>
2026-07-13 22:01:48 +0600

    creating db and executing query and fetching the data form db

--- demo/index.php
+++ demo/index.php
@@ -1,6 +1,21 @@
 <?php
 include_once 'functions.php';
-include_once 'router.php';
+// include_once 'router.php';
+
+$dns = 'mysql:host=localhost;port=3306;user=laravel_user;dbname=laravel_db;charset=utf8mb4;password=Secret123!;';
+$pdo = new PDO($dns);
+$statement = $pdo->prepare('select * from posts');
+$statement->execute();
+// $posts = $statement->fetchAll();
+$posts = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+// dd($posts);
+foreach ($posts as $post) :?>
 
 
 
+<ul>
+    <li><?=$post['title']?></li>
+</ul>
+
+<?php endforeach?>
