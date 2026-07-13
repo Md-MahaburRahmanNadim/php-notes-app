6b09ec3dd12a6b88eb13952c512e276f59531375
Md-MahaburRahmanNadim <mdnadimhaaan120@gmail.com>
2026-07-13 16:10:15 +0600

    route full refectoring

--- demo/index.php
+++ demo/index.php
@@ -4,22 +4,31 @@ include_once 'functions.php';
 $fullUri = $_SERVER['REQUEST_URI'];
 // to get only url (path) not the query parms
 $uri = parse_url($fullUri)['path'];
-// dd(parse_url($uri)['path']);
 
-// if($uri === '/'){
-//     require_once './controllers/index.php';
-// }elseif($uri === '/about'){
-//     require_once './controllers/about.php';
-// }elseif($uri === '/contract'){
-//     require_once './controllers/contract.php';
-// }
 $routes =[
     '/'=>'controllers/index.php',   
     '/about'=>'controllers/about.php',
     '/contract'=>'controllers/contract.php'
     ];
 
-    // if(array_key_exists($key,$routes))
+function abort($code){
+    http_response_code($code=404);
+    require_once "views/{$code}.php";
+    die();
+}
+function routeToController($uri,$routes){
     if(array_key_exists($uri,$routes)){
+
         require_once $routes[$uri];
+
+    }else{
+
+        abort();
+
     }
+
+}
+routeToController($uri,$routes);
+
+
+
---
+++ demo/views/404.php
@@ -0,0 +1,16 @@
+ 
+<?php
+include_once 'partials/head.php';
+include_once 'partials/nav.php';
+include_once 'partials/banner.php';
+?>
+
+   
+  
+   <div>
+    <p>Sorry the page is not found</p>
+    <a href="/">Go to home</a>
+   </div>
+    
+   <?php include_once 'partials/footer.php'
+   ?>
