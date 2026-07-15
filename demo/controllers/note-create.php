9dd172ece79177a3eb1be15e2c37599da7f4e37f
Md-MahaburRahmanNadim <mdnadimhaaan120@gmail.com>
2026-07-15 16:39:23 +0600

    sending data to server but the data is not senitize when viewing

--- demo/controllers/note-create.php
+++ demo/controllers/note-create.php
@@ -2,8 +2,10 @@
 $heading = 'Create Note';
 $config = include_once 'config.php';
 $db =new Database($config['database']);
-
+$query = "INSERT INTO notes (body,user_id) VALUES (:body,:user_id)";
 if($_SERVER["REQUEST_METHOD"]==='POST'){
+$body = $_POST['body'];
+$db->query($query,['body'=>$body,'user_id'=>11]);
 }
 
 
