9ea068838f98a35d46809873abf4b57be2b4be8f
Md-MahaburRahmanNadim <mdnadimhaaan120@gmail.com>
2026-07-15 10:04:48 +0600

    handilling the authorizition. if user not authorize then 403 if not found then 404

--- demo/controllers/note.php
+++ demo/controllers/note.php
@@ -2,11 +2,22 @@
 $config = include_once 'config.php';
 
 $db= new Database($config['database']);
-
+$courrentUser = 11;
 $heading = 'My Notes';
 $id = $_GET['id'];
 $note = $db->query("select * from notes where id =:id",[':id'=>$id])->fetch();
 // dd($note);
+// if the note data not found in db the show 404 not found page
+if(!$note){
+    abort();
+}
+
+// if data found in db but the user is not authorize then show 403 unauthorized page
+
+if($note['user_id'] !== $courrentUser){
+    abort(403);
+}
+
 
 include_once './views/note.view.php';
 
---
+++ demo/views/403.php
@@ -0,0 +1,16 @@
+ 
+<?php
+include_once 'partials/head.php';
+include_once 'partials/nav.php';
+include_once 'partials/banner.php';
+?>
+
+   
+  
+   <div>
+    <p>Sorry! You are unauthorized to view this page</p>
+    <a href="/">Go to home</a>
+   </div>
+    
+   <?php include_once 'partials/footer.php'
+   ?>
