219059aaf87900e1ba99fb8f069c90d877b73c16
Md-MahaburRahmanNadim <mdnadimhaaan120@gmail.com>
2026-07-19 10:24:51 +0600

    auto load class and create a Core dir to store core file of our project

--- demo/controllers/notes/create.php
+++ demo/controllers/notes/create.php
@@ -2,12 +2,7 @@
 include_once base_path('Validator.php');
 $config = include_once base_path('config.php');
 $db = new Database($config['database']);
-/**
- * in table plus we in history tag we can see how the data is inserted in a table. by inserting a data
- * 
- * from there we can write a insert query like below
- * 
- */
+
 $errors = [];
 
 $query = "INSERT INTO notes (body,user_id) VALUES (:body,:user_id)";
--- demo/public/index.php
+++ demo/public/index.php
@@ -1,19 +1,13 @@
 <?php
 const BASE_PATH = __DIR__ . '/../';
 // include_once base_path('functions.php');
-include_once BASE_PATH . 'functions.php';
-include_once base_path('Database.php');
-include_once base_path('Response.php');
-
-include_once base_path('router.php');
-// var_dump(__DIR__ . '/../'); // its will start from demo (project root);
-
-/**
- * we need to change our root directory (from ) project root to (public ) folder 
- * 
- * Because now we can access each and every file by edeting url. which is super bad
- * 
- * public folder only contain (index.php and js,css, assets) etc
- * 
- * php -S nadim:8888 -t public
- */
+include_once BASE_PATH . 'Core/' . 'functions.php';
+include_once base_path('Core/' . 'Response.php');
+spl_autoload_register(function ($class) {
+    // include_once base_path('Database.php');
+    /**
+     * every class that is create a instance will be include by automatically (when we create(inisitated) a class)
+     */
+    include_once base_path('Core/' . $class . '.php');
+});
+include_once base_path('Core/' . 'router.php');
